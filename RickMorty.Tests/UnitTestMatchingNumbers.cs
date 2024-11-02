70e64bbe4f9f18885723f1cd60e62247af5b6dc2
githubninpoka <github@ninpoka.nl>
2024-11-02 06:58:36 +0100

    added some comments about possible unit tests

--- RickMorty.Tests/UnitTestMatchingNumbers.cs
+++ RickMorty.Tests/UnitTestMatchingNumbers.cs
@@ -8,6 +8,12 @@ using RickMorty.ConsoleAppRetrieveAndStoreData;
 
 public class Tests
 {
+    // for a more difficult test I would probably load some manual data with a NULL externalId into the database
+    // then remove all entries except that manual entry
+    // then verify the numbers (external api vs entries in db with externalIds) are the same
+    // why?
+    // the developer exercise now asks to 'empty' the database through the console app
+    // but in a more realistic scenario we would possibly leave the manual entries alone when deleting data, depending on business logic.
 
     [SetUp]
     public void Setup()
